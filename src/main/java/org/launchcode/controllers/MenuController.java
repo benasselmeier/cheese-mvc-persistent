6225fb9da383feee44b1af15f4a5dcdfd54fedea
Ben <benasselmeier@gmail.com>
2018-06-23 19:08:36 -0500

    Done for today. Stopped at "Add Menu Items" header on assignment page.
    Almost done!

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,12 +1,19 @@
 package org.launchcode.controllers;
 
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "menu")
@@ -27,5 +34,33 @@ public class MenuController {
         return "menu/index";
     }
 
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Menu());
+        model.addAttribute("title", "Add Menu");
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            model.addAttribute(new Menu());
+            return "menu/add";
+        }
+        else {
+            menuDao.save(menu);
+            return "redirect:view/" + menu.getId();
+        }
+
+    }
+
+    @RequestMapping(value = "view", method = RequestMethod.GET)
+    public String viewMenu (Model model, @PathVariable int menuId) {
+        model.addAttribute("menu", menuDao.findOne(menuId));
+        model.addAttribute("title", "View Menu");
+        return "view";
+    }
 
 }
